3c1c0f73fa8389fcfbe1dd72fdbd73a1ac71ff03
Yuriy Kopylov <iyurij@gmail.com>
2016-11-06 16:59:40 +1100

    Created project structure for new project.

---
+++ CloudAppBrowser.Views/MainForm.xeto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+
+namespace CloudAppBrowser.Views
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            XamlReader.Load(this);
+        }
+    }
+}
---
+++ CloudAppBrowser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CloudAppBrowser.Views;
+using Eto;
+using Eto.Forms;
+
+namespace CloudAppBrowser
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application application = new Application(Platform.Detect);
+
+            MainForm mainForm = new MainForm();
+
+            application.Run(mainForm);
+        }
+    }
+}
