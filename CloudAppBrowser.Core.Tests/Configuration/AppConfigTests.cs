fb0d32041f7976d71a20bacf241bd083e0b93b3b
Yuriy Kopylov <iyurij@gmail.com>
2016-11-12 14:25:25 +1100

    Added configuration format.

---
+++ CloudAppBrowser.Core.Tests/Configuration/AppConfigTests.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using CloudAppBrowser.Core.Configuration;
+using NUnit.Framework;
+
+namespace CloudAppBrowser.Core.Tests.Configuration
+{
+    [TestFixture]
+    public class AppConfigTests
+    {
+        [Test]
+        public void TestSmoke()
+        {
+            AppConfig appConfig = new AppConfig
+            {
+                Environments =
+                {
+                    new EnvironmentConfig
+                    {
+                        Name = "Env1",
+                        Services =
+                        {
+                            new ServiceConfig
+                            {
+                                Name = "Eureka1",
+                                Eureka = new EurekaServiceConfig {Url = "http://xxx.yyy:123"}
+                            },
+                            new ServiceConfig
+                            {
+                                Name = "Eureka2",
+                                Eureka = new EurekaServiceConfig {Url = "http://xxx.yyy:123"}
+                            },
+                            new ServiceConfig
+                            {
+                                Name = "Docker",
+                                Docker = new DockerServiceConfig {Url = "http://xxx.yyy:123"}
+                            }
+                        }
+                    },
+                    new EnvironmentConfig
+                    {
+                        Name = "Env2",
+                        Services =
+                        {
+                            new ServiceConfig
+                            {
+                                Name = "Docker",
+                                Docker = new DockerServiceConfig {Url = "http://xxx.yyy:123"}
+                            }
+                        }
+                    }
+                }
+            };
+
+            string config;
+            using (StringWriter writer = new StringWriter())
+            {
+                AppConfig.WriteTo(writer, appConfig);
+                config = writer.ToString();
+            }
+
+            System.Console.WriteLine(config);
+
+            AppConfig appConfig2;
+            using (StringReader reader = new StringReader(config))
+            {
+                appConfig2 = AppConfig.ReadFrom(reader);
+            }
+
+            Assert.That(appConfig2.Environments.Count, Is.EqualTo(appConfig.Environments.Count));
+            Assert.That(appConfig2.Environments[0].Services.Count, Is.EqualTo(appConfig.Environments[0].Services.Count));
+            Assert.That(appConfig2.Environments[0].Services[0].Name, Is.EqualTo(appConfig.Environments[0].Services[0].Name));
+        }
+    }
+}
---
+++ CloudAppBrowser.Core/Configuration/AppConfig.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace CloudAppBrowser.Core.Configuration
+{
+    public class AppConfig
+    {
+        public List<EnvironmentConfig> Environments { get; set; } = new List<EnvironmentConfig>();
+
+        public static void WriteTo(TextWriter writer, AppConfig config)
+        {
+            Serializer serializer = new SerializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+            serializer.Serialize(writer, config);
+        }
+
+        public static AppConfig ReadFrom(TextReader reader)
+        {
+            Deserializer deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+            return deserializer.Deserialize<AppConfig>(reader);
+        }
+    }
+
+    public class EnvironmentConfig
+    {
+        public string Name { get; set; }
+        public List<ServiceConfig> Services { get; set; } = new List<ServiceConfig>();
+    }
+
+    public class ServiceConfig
+    {
+        public string Name { get; set; }
+        public EurekaServiceConfig Eureka { get; set; }
+        public DockerServiceConfig Docker { get; set; }
+    }
+
+    public class EurekaServiceConfig
+    {
+        public string Url { get; set; }
+    }
+
+    public class DockerServiceConfig
+    {
+        public string Url { get; set; }
+    }
+}
