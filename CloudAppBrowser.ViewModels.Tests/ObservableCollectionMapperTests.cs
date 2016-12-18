044fd482aece6f6f0e75330373e05df90257419d
Yuriy Kopylov <iyurij@gmail.com>
2016-12-18 17:43:07 +1100

    Added ObservableCollectionMapper.

---
+++ CloudAppBrowser.ViewModels.Tests/ObservableCollectionMapperTests.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CloudAppBrowser.ViewModels.Tests
+{
+    [TestFixture]
+    public class ObservableCollectionMapperTests
+    {
+        private readonly Random random = new Random();
+
+        [Test]
+        public void Test()
+        {
+            TestCase(
+                new string[] {},
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[]
+                {
+                    "Add: {null} -> {0: 1}",
+                    "Add: {null} -> {1: 2}",
+                    "Add: {null} -> {2: 3}",
+                    "Add: {null} -> {3: 4}",
+                    "Add: {null} -> {4: 5}"
+                },
+                new string[] {}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {},
+                new string[] {}
+                );
+
+            TestCase(
+                new string[] {"2", "4"},
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[]
+                {
+                    "Add: {null} -> {0: 1}",
+                    "Add: {null} -> {2: 3}",
+                    "Add: {null} -> {4: 5}"
+                },
+                new string[] {}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"2", "4"},
+                new string[]
+                {
+                    "Remove: {4: 5} -> {null}",
+                    "Remove: {2: 3} -> {null}",
+                    "Remove: {0: 1} -> {null}"
+                },
+                new string[] {"1", "3", "5"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"-1", "-2", "3", "-4", "-5"},
+                new string[]
+                {
+                    "Replace: {0: 1} -> {0: -1}",
+                    "Replace: {1: 2} -> {1: -2}",
+                    "Replace: {3: 4} -> {3: -4}",
+                    "Replace: {4: 5} -> {4: -5}",
+                },
+                new string[] {"1", "2", "4", "5"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"2", "3", "4", "5", "1"},
+                new string[]
+                {
+                    "Move: {0: 1} -> {4: 1}"
+                },
+                new string[] {}
+                );
+
+            TestCase(
+                new string[] {"2", "3", "4", "5", "1"},
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[]
+                {
+                    "Move: {4: 1} -> {0: 1}"
+                },
+                new string[] {}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"-1", "5", "2", "3", "4"},
+                new string[]
+                {
+                    "Replace: {0: 1} -> {0: -1}",
+                    "Move: {4: 5} -> {1: 5}",
+                },
+                new string[] {"1"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"5", "1", "2", "3", "-4"},
+                new string[]
+                {
+                    "Replace: {3: 4} -> {3: -4}",
+                    "Move: {4: 5} -> {0: 5}"
+                },
+                new string[] {"4"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"5", "1", "2", "3"},
+                new string[]
+                {
+                    "Remove: {3: 4} -> {null}",
+                    "Move: {3: 5} -> {0: 5}",
+                },
+                new string[] {"4"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5"},
+                new string[] {"3", "4", "5", "1"},
+                new string[]
+                {
+                    "Remove: {1: 2} -> {null}",
+                    "Move: {0: 1} -> {3: 1}"
+                },
+                new string[] {"2"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5", "6", "7"},
+                new string[] {"7", "-2", "4", "3", "-5", "6", "1"},
+                null,
+                new string[] {"2", "5"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5", "6", "7"},
+                new string[] {"7", "-2", "4", "3", "-5", "6", "0", "1"},
+                null,
+                new string[] {"2", "5"}
+                );
+
+            TestCase(
+                new string[] {"1", "2", "3", "4", "5", "6", "7"},
+                new string[] {"7", "4", "3", "6", "1"},
+                null,
+                new string[] {"2", "5"}
+                );
+        }
+
+        [Test]
+        [Explicit]
+        public void TestPerformance()
+        {
+            const int iterationCount = 1000;
+            const int availableValuesCount = 2000;
+            const int sourceListSize = 1000;
+            const int destListSize = 1000;
+
+            Stopwatch stopwatch = new Stopwatch();
+            long operationCount = 0;
+
+            for (int it = 0; it < iterationCount; it++)
+            {
+                List<string> sourceList = GenerateSet(sourceListSize, availableValuesCount, 0.5, 3);
+                List<string> destList = GenerateSet(destListSize, availableValuesCount, 0.65, 1);
+
+                ObservableCollection<string> collection = new ObservableCollection<string>();
+
+                ObservableCollectionMapper<string, string> mapper = new ObservableCollectionMapper<string, string>
+                    (
+                    entity => entity,
+                    model => model,
+                    (entity, model) => { },
+                    null
+                    );
+
+                mapper.UpdateCollection(sourceList, collection);
+
+                collection.CollectionChanged += (sender, args) => operationCount++;
+
+                stopwatch.Start();
+                mapper.UpdateCollection(destList, collection);
+                stopwatch.Stop();
+
+                Assert.That(collection, Is.EqualTo(destList));
+            }
+
+            Console.WriteLine("Update Time:     {0} ms ({1} ms per iteration)", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedMilliseconds/iterationCount);
+            Console.WriteLine("Operation Count: {0} ({1} per iteration)", operationCount, operationCount/iterationCount);
+        }
+
+        private List<string> GenerateSet(int listSize, int availableValuesCount, double pFirst, int numFirst)
+        {
+            List<string> values = new List<string>();
+            for (int i = 0; i < availableValuesCount; i++)
+            {
+                values.Add(i.ToString());
+            }
+            List<string> res = new List<string>();
+            for (int i = 0; i < listSize; i++)
+            {
+                int range = values.Count;
+                if (random.NextDouble() < pFirst)
+                {
+                    range = Math.Min(range, numFirst);
+                }
+                int idx = random.Next(range);
+                res.Add(values[idx]);
+                values.RemoveAt(idx);
+            }
+            return res;
+        }
+
+        private void TestCase(string[] sourceContent, string[] destinationContent, string[] expectedOperations, string[] expectedDisposals)
+        {
+            List<string> disposals = new List<string>();
+
+            ObservableCollectionMapper<string, SimpleViewModel> mapper = new ObservableCollectionMapper<string, SimpleViewModel>(
+                entity => new SimpleViewModel(disposals, entity, "D-" + entity),
+                model => model.Entity,
+                (entity, model) => { },
+                null);
+
+            ObservableCollection<SimpleViewModel> target = new ObservableCollection<SimpleViewModel>();
+
+            mapper.UpdateCollection(sourceContent, target);
+            Assert.That(disposals, Is.Empty);
+
+            List<string> operations = new List<string>();
+            target.CollectionChanged += (sender, args) => operations.Add(FormatChangeEvent(args));
+
+            mapper.UpdateCollection(destinationContent, target);
+            Assert.That(target.Select(m => m.Entity).ToList(), Is.EqualTo(destinationContent));
+            if (expectedOperations != null)
+            {
+                Assert.That(operations, Is.EqualTo(expectedOperations), "operations does not match expectations");
+            }
+            Assert.That(disposals, Is.EquivalentTo(expectedDisposals), "disposals does not match expectations");
+        }
+
+        private string FormatChangeEvent(NotifyCollectionChangedEventArgs args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(args.Action);
+            sb.Append(": {");
+            sb.Append(FormatItems(args.OldStartingIndex, args.OldItems));
+            sb.Append("} -> {");
+            sb.Append(FormatItems(args.NewStartingIndex, args.NewItems));
+            sb.Append("}");
+            return sb.ToString();
+        }
+
+        private string FormatItems(int startingIndex, IList items)
+        {
+            if (items == null)
+            {
+                return "null";
+            }
+
+            List<string> itemNames = new List<string>();
+            foreach (object item in items)
+            {
+                itemNames.Add(item.ToString());
+            }
+            return startingIndex + ": " + string.Join(", ", itemNames);
+        }
+
+        private class SimpleViewModel : IDisposable
+        {
+            private readonly List<string> disposals;
+
+            public string Entity { get; }
+            public string Data { get; }
+
+            public SimpleViewModel(List<string> disposals, string entity, string data)
+            {
+                this.disposals = disposals;
+                Entity = entity;
+                Data = data;
+            }
+
+            public void Dispose()
+            {
+                disposals.Add(Entity);
+            }
+
+            public override string ToString()
+            {
+                return Entity;
+            }
+        }
+    }
+}
---
+++ CloudAppBrowser.ViewModels/ObservableCollectionMapper.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CloudAppBrowser.ViewModels
+{
+    public class ObservableCollectionMapper<TEntity, TViewModel>
+    {
+        private readonly ConstructModel constructModel;
+        private readonly GetEntity getEntity;
+        private readonly UpdateModel updateModel;
+        private readonly Comparison<TViewModel> comparison;
+
+        public ObservableCollectionMapper(ConstructModel constructModel, GetEntity getEntity, UpdateModel updateModel, Comparison<TViewModel> comparison)
+        {
+            this.constructModel = constructModel;
+            this.getEntity = getEntity;
+            this.updateModel = updateModel;
+            this.comparison = comparison;
+        }
+
+        public delegate TViewModel ConstructModel(TEntity entity);
+
+        public delegate TEntity GetEntity(TViewModel model);
+
+        public delegate void UpdateModel(TEntity entity, TViewModel model);
+
+        public void UpdateCollection(IEnumerable<TEntity> entities, ObservableCollection<TViewModel> target)
+        {
+            Dictionary<TEntity, TViewModel> existingModels = target.ToDictionary(model => getEntity(model));
+
+            List<TViewModel> presentModels = new List<TViewModel>();
+
+            foreach (TEntity entity in entities)
+            {
+                TViewModel model;
+                if (existingModels.TryGetValue(entity, out model))
+                {
+                    updateModel(entity, model);
+                }
+                else
+                {
+                    model = constructModel(entity);
+                }
+                presentModels.Add(model);
+            }
+
+            if (comparison != null)
+            {
+                presentModels.Sort(comparison);
+            }
+
+            List<TViewModel> removedModels = UpdateTargetContent(presentModels, target);
+            ReorderTarget(presentModels, target);
+
+            foreach (TViewModel removedModel in removedModels)
+            {
+                IDisposable disposableModel = removedModel as IDisposable;
+                disposableModel?.Dispose();
+            }
+        }
+
+        private List<TViewModel> UpdateTargetContent(List<TViewModel> presentModels, ObservableCollection<TViewModel> target)
+        {
+            Dictionary<TViewModel, int> presentModelIndices = new Dictionary<TViewModel, int>();
+            for (int i = 0; i < presentModels.Count; i++)
+            {
+                presentModelIndices.Add(presentModels[i], i);
+            }
+
+            HashSet<TViewModel> existingModels = new HashSet<TViewModel>();
+            foreach (TViewModel model in target)
+            {
+                existingModels.Add(model);
+            }
+
+            List<TViewModel> longestOrderedChain = FindLongestOrderedChain(presentModels, presentModelIndices, target);
+
+            List<TViewModel> removedModels = new List<TViewModel>();
+            int insertPosition = 0;
+            int sourcePosition = 0;
+            Queue<int> replacePositions = new Queue<int>();
+            foreach (TViewModel fixedModel in longestOrderedChain)
+            {
+                replacePositions.Clear();
+                while (!target[insertPosition].Equals(fixedModel))
+                {
+                    if (!presentModelIndices.ContainsKey(target[insertPosition]))
+                    {
+                        replacePositions.Enqueue(insertPosition);
+                    }
+                    insertPosition++;
+                }
+                while (!presentModels[sourcePosition].Equals(fixedModel))
+                {
+                    if (existingModels.Contains(presentModels[sourcePosition]))
+                    {
+                        sourcePosition++;
+                    }
+                    else if (replacePositions.Count > 0)
+                    {
+                        int replacePosition = replacePositions.Dequeue();
+                        removedModels.Add(target[replacePosition]);
+                        target[replacePosition] = presentModels[sourcePosition];
+                        sourcePosition++;
+                    }
+                    else
+                    {
+                        target.Insert(insertPosition, presentModels[sourcePosition]);
+                        insertPosition++;
+                        sourcePosition++;
+                    }
+                }
+                sourcePosition++;
+                insertPosition++;
+            }
+            replacePositions.Clear();
+            while (insertPosition < target.Count)
+            {
+                if (!presentModelIndices.ContainsKey(target[insertPosition]))
+                {
+                    replacePositions.Enqueue(insertPosition);
+                }
+                insertPosition++;
+            }
+            while (sourcePosition < presentModels.Count)
+            {
+                if (existingModels.Contains(presentModels[sourcePosition]))
+                {
+                    sourcePosition++;
+                }
+                else if (replacePositions.Count > 0)
+                {
+                    int replacePosition = replacePositions.Dequeue();
+                    removedModels.Add(target[replacePosition]);
+                    target[replacePosition] = presentModels[sourcePosition];
+                    sourcePosition++;
+                }
+                else
+                {
+                    target.Insert(insertPosition, presentModels[sourcePosition]);
+                    insertPosition++;
+                    sourcePosition++;
+                }
+            }
+            int removePosition = target.Count - 1;
+            while (removePosition >= 0)
+            {
+                if (!presentModelIndices.ContainsKey(target[removePosition]))
+                {
+                    removedModels.Add(target[removePosition]);
+                    target.RemoveAt(removePosition);
+                }
+                removePosition--;
+            }
+            return removedModels;
+        }
+
+        private void ReorderTarget(List<TViewModel> presentModels, ObservableCollection<TViewModel> target)
+        {
+            Dictionary<TViewModel, int> presentModelIndices = new Dictionary<TViewModel, int>();
+            for (int i = 0; i < presentModels.Count; i++)
+            {
+                presentModelIndices.Add(presentModels[i], i);
+            }
+
+            List<TViewModel> longestOrderedChain = FindLongestOrderedChain(presentModels, presentModelIndices, target);
+            List<int> longestOrderedIndexChain = longestOrderedChain.Select(m => presentModelIndices[m]).ToList();
+
+            for (int index = 0; index < presentModels.Count; index++)
+            {
+                TViewModel model = target[index];
+                int presentModelIndex = presentModelIndices[model];
+                var insertPosition = FindInsertPosition(longestOrderedIndexChain, presentModelIndex);
+                if (insertPosition < longestOrderedIndexChain.Count && longestOrderedIndexChain[insertPosition] == presentModelIndex)
+                {
+                    continue;
+                }
+                int newIndex = insertPosition < longestOrderedIndexChain.Count ? target.IndexOf(presentModels[longestOrderedIndexChain[insertPosition]]) : target.Count;
+                if (newIndex > index)
+                {
+                    target.Move(index, newIndex - 1);
+                    index--;
+                }
+                else
+                {
+                    target.Move(index, newIndex);
+                }
+                longestOrderedIndexChain.Insert(insertPosition, presentModelIndices[model]);
+            }
+        }
+
+        private static List<TViewModel> FindLongestOrderedChain(
+            List<TViewModel> presentModels,
+            Dictionary<TViewModel, int> presentModelIndices,
+            ObservableCollection<TViewModel> target)
+        {
+            Dictionary<int, int> parents = new Dictionary<int, int>();
+            List<int> lastElementByLength = new List<int>();
+            foreach (TViewModel model in target)
+            {
+                int modelIndex;
+                if (!presentModelIndices.TryGetValue(model, out modelIndex))
+                {
+                    continue;
+                }
+
+                int insertPosition = FindInsertPosition(lastElementByLength, modelIndex);
+
+                if (insertPosition >= lastElementByLength.Count)
+                {
+                    lastElementByLength.Add(modelIndex);
+                }
+                else
+                {
+                    lastElementByLength[insertPosition] = modelIndex;
+                }
+                if (insertPosition > 0)
+                {
+                    parents.Add(modelIndex, lastElementByLength[insertPosition - 1]);
+                }
+            }
+            List<TViewModel> longestChain = new List<TViewModel>();
+            if (lastElementByLength.Count > 0)
+            {
+                int modelIndex = lastElementByLength[lastElementByLength.Count - 1];
+                longestChain.Add(presentModels[modelIndex]);
+                while (parents.TryGetValue(modelIndex, out modelIndex))
+                {
+                    longestChain.Add(presentModels[modelIndex]);
+                }
+            }
+            longestChain.Reverse();
+            return longestChain;
+        }
+
+        /// <summary>
+        /// Finds the position within the given sorted array where the given element should be inserted using binary search.
+        /// If the element is already in the array, returns its position.
+        /// </summary>
+        /// <param name="values">The sorted array.</param>
+        /// <param name="value">The value to look for.</param>
+        private static int FindInsertPosition(List<int> values, int value)
+        {
+            int low = 0;
+            int hi = values.Count - 1;
+            while (low <= hi)
+            {
+                int mid = (hi + low)/2;
+                if (values[mid] < value)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
+            return low;
+        }
+    }
+}
