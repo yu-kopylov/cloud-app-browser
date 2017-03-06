3f08037f5091a5936ce33de3f4e41e5db285c143
Yuriy Kopylov <iyurij@gmail.com>
2017-03-06 23:40:12 +1100

    Fixed NPE in DockerService.

--- CloudAppBrowser.Core/Services/Docker/DockerService.cs
+++ CloudAppBrowser.Core/Services/Docker/DockerService.cs
@@ -111,7 +111,7 @@ namespace CloudAppBrowser.Core.Services.Docker
                     }
                     dockerImage.Id = responseImage.ID;
                     dockerImage.Created = responseImage.Created;
-                    dockerImage.RepoTags = new List<string>(responseImage.RepoTags);
+                    dockerImage.RepoTags = new List<string>(responseImage.RepoTags ?? Enumerable.Empty<string>());
                     dockerImage.Size = responseImage.Size;
                     dockerImage.VirtualSize = responseImage.VirtualSize;
                 }
